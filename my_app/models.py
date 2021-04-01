5efd135a8d351c6712f7fcb0b1d97870e07ca94e
shiv saxena <shivsaxena@hotmail.com>
2021-04-01 15:31:45 -0700

    Initial commit

---
+++ my_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta: # Handle plural of Search
+        verbose_name_plural = "Searches"
